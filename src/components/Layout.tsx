76378fae05c3e6a6a87f997dc101c067e33d4937
Dinius05 <dino.reginato@gmail.com>
2026-02-17 15:40:13 -0300

    Trigger_new_build

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -102,7 +102,7 @@ export const Layout: React.FC = () => {
                 >
                   <LogOut className="mr-1 h-3 w-3" /> Cerrar Sesión
                 </button>
-                <div className="text-[10px] text-gray-400 mt-1">v1.4 (Updates)</div>
+                <div className="text-[10px] text-gray-400 mt-1">v1.4.1 (Retry Build)</div>
               </div>
             </div>
           </div>
